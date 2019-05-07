2c9c5e78a0ac9e075c22cb0054d7541b5e5febb0
joshuayen <joshuayen@gmail.com>
2019-05-07 09:04:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-
-bundle exec ruby app.rb
+ #!/bin/bash
+ if [ ! -d "/var/lib/mysql/cacti" ]; then
+    mv /var/lib/mysql.backup/* /var/lib/mysql/
+ fi
+ 
+ if [ ! -d "/opt/cacti/docs" ]; then
+    mv /opt/cacti.backup/* /opt/cacti/
+ fi
+ 
+ /sbin/my_init
